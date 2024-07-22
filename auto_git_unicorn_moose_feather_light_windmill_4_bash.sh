1ee220a1fe19069f6570bfbc905bf95a689e9d5b
essingen123 <essingen123@gmail.com>
2024-07-23 01:34:46 +0200

    Syncing changes with GitHub

--- auto_git_unicorn_moose_feather_light_windmill_4_bash.sh
+++ auto_git_unicorn_moose_feather_light_windmill_4_bash.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#file:
+#auto_git_unicorn_moose_feather_light_windmill_4_bash.sh
+
 # Main script to orchestrate other scripts
 # Include other scripts
 source ./config_handler.sh
@@ -7,6 +10,12 @@ source ./error_handler.sh
 source ./github_setup.sh
 source ./git_operations.sh
 source ./bashrc_update.sh
+
+# Function to update the GitHub About section
+update_github_about() {
+    log "Updating the GitHub About section..."
+    python3 "${script_dir}/update_github_about.py"
+}
 
 # Main script logic
 read_config
@@ -31,4 +40,8 @@ else
     log "If you wish to also have that cool HTML page, you can run the following command to generate a neat webpage for your GitHub project: ./_extra_bonus.py"
 fi
 
+# Ensure the script directory variable is set correctly
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Call the function to update the GitHub About section
 update_github_about
